0d53f628a47f79f38e55a1f7f0300ca879b0313c
Your Name <maggiesamwells@gmail.com>
2025-06-11 20:19:15 +0300

    js implement

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solutions in this file
 function writeCards(names, eventName) {
-  const messages = [];
-  for (let i = 0; i < names.length; i++) {
-    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
-  }
-  return messages;
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
 }
 
 function countDown(startingNumber) {
